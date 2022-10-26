773e5e5e5c8dfe350982f8ac33cdbf4b80785ede
Nliker <tjwjdgus83@naver.com>
2022-10-26 14:24:26 +0900

    아키텍쳐 설계

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy import create_engine
+
 
 def create_app(test_config=None):
     app=Flask(__name__)
@@ -12,4 +14,11 @@ def create_app(test_config=None):
     else:
         app.config.update(test_config)
     
+    database=create_engine(app.config['DB_URL'],encoding='utf-8',max_overflow=0)
+    print("데이터베이스 연결 성공!")
+
+    @app.route("/ping",methods=["GET"])
+    def ping():
+        return "pong",200
     
+    return app
